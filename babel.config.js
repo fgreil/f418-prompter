6c3e99ec08ed66e7a8efa7f2f5c6317436e093de
fgreil <f.greil@tum.de>
2025-12-10 20:58:42 +0100

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
+/**
+ * Babel Configuration
+ * 
+ * Configures Babel transpiler for React Native with Expo
+ * The reanimated plugin enables animated gestures and transitions
+ */
+
 module.exports = function(api) {
   api.cache(true);
   return {
